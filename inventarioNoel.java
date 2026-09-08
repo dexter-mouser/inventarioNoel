f0d9131299598df57763fab00817881a4a77d88a
Dexter <josembotero17qgmail.com>
2026-09-07 22:14:57 -0500

    feat: switch 1erparte

--- inventarioNoel.java
+++ inventarioNoel.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class InventarioNoel {
     public static void main(String[] args) {
         String[] inventario = {"ducales", "festival", "tosh", null, null};
@@ -21,5 +23,33 @@ public class InventarioNoel {
             opcion = leer.nextInt();
             //
         }
+            switch (opcion) {
+                case 1:
+                    System.out.println("1. Listar Producto");
+                    for (String producto : inventario) {
+                        if (producto != null) {
+                            System.out.println(producto);
+                        }
+                    }
+                    break;
+                case 2:
+                    System.out.println("Nombre del producto a buscar:  ");
+                    System.out.println("(Todo en minuscula y sin caracteres especiales)");
+                    nombreBuscado = leer.nextLine();
+                    //
+                    encontrado = buscarProducto(inventario, nombreBuscado);
+                    if (encontrado) {
+                        System.out.println("Producto encontrado: " + nombreBuscado);
+                    } else {
+                        System.out.println("Producto no encontrado: " + nombreBuscado);
+                    }
+                    break;
+                case 3:
+                    System.out.print("Nombre del nuevo producto:  ");
+                    System.out.println("(Todo en minuscula y sin caracteres especiales)");
+                    nuevoProducto = leer.nextLine();
+                    
+                case 4:
+            }
     }
     }
